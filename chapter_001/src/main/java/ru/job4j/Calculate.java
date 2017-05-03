e59ce2f2a3547b089a10ba1b8f26bd5bb1d6c9cc
supportingII <slavikman@bk.ru>
2017-05-03 10:29:31 +0300

    task 4086

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j;
 
+/**Класс для арифмитических вычислений
+* @author Vyacheslav Tikhonov (mailto:slavikman@bk.ru)
+* @date 03.05.2017
+* @vercion 1.0
+*/
+
 public class Calculate{
-	
+	/**Вывод текста
+	* @param args - args	
+	*/
 	public static void main (String[] args){
 		
 		System.out.println("Hello world.");
