2fb25ac70d372c269123f0b6ee2fa363e15b6697
supportingII <slavikman@bk.ru>
2017-06-22 10:14:24 +0300

    22/06/2017

--- chapter_001/src/main/java/ru/job4j/array/RotateArray.java
+++ chapter_001/src/main/java/ru/job4j/array/RotateArray.java
@@ -6,19 +6,18 @@ package ru.job4j.array;
 
 public class RotateArray {
 	/**
-	* Метод переворачивает массива.
+	* Метод переворачивает массив.
 	* @param array принимаемый двумерный массив.
 	* @return array возвращаем перевернутый массив.
 	*/
-	public int[][] rotate(int[][] array) {
-		int temp; //Параметр для хранения первого значения
+	public int[][] rotate(int[][] array) {		
 		for (int z = 0; z < array.length / 2; z++) {
 			int	u = array.length - 1 - z; //Изменяемый(в цикле)
 			int x = array.length - 1 - z; //Статичный (в цикле)
 			int m = 0 + z; //Статичный (в цикле)
 			int a = 0 + z; //Изменяемый(в цикле)
 			for (; a <= array.length - 2 - z; a++) {
-				temp = array[m][a];
+				int temp = array[m][a];
 				array[m][a] = array[u][m];
 				array[u][m] = array[x][u];
 				array[x][u] = array[a][x];
@@ -29,68 +28,3 @@ public class RotateArray {
 		return array;
 	}
 }
-
-
-/**
-	public int[][] rotate(int[][] array) {
-		int rememberOne = 0, rememberTwo = 0, z;
-        boolean first;
-		// Цикл определяющий кол-во проходов по всему массиву.
-		// Проход делаем по часовой стрелке.
-        for (int a = 1; a <= array.length - 1; a++) {
-            first  = true;
-            z = 0;
-			// Цикл отвечающий за верхнюю грань и первый проход
-            for (int b = 0; b <= array.length - 2; b++) {
-                rememberOne = rememberTwo;
-                rememberTwo = array[z][b + 1];
-                if (first) {
-                    array[z][b + 1] = array[z][b];
-                    first  = false;
-                } else {
-                    array[z][b + 1] = rememberOne;
-                }
-            }
-			// Цикл отвечающий за правую грань
-            z = array.length - 1;
-            for (int b = 1; b <= array.length - 1; b++) {
-                rememberOne = rememberTwo;
-                rememberTwo = array[b][z];
-                array[b][z] = rememberOne;
-            }
-			// Цикл отвечающий за нижнюю грань
-            z = array.length - 1;
-            for (int b = array.length - 2; b >= 0; b--) {
-                rememberOne = rememberTwo;
-                rememberTwo = array[z][b];
-                array[z][b] = rememberOne;
-            }
-			// Цикл отвечающий за левую грань
-            z = 0;
-            for (int b = array.length - 2; b >= 0; b--) {
-                rememberOne = rememberTwo;
-                rememberTwo = array[b][z];
-                array[b][z] = rememberOne;
-            }
-        }
-        return array;
-	}
-
-	public class RotateArray {
-	public int[][] rotate(int[][] array) {
-		int temp; //Параметр для хранения первого значения
-		int x = array.length - 1; //Статичные параметры
-		int	u = array.length - 1; //Изменяемые параметры
-			for (int a = 0; a <= array.length - 2; a++) {
-				temp = array[0][a];
-				array[0][a] = array[u][0];
-				array[u][0] = array[x][u];
-				array[x][u] = array[a][x];
-				array[a][x] = temp;
-				u--;
-			}
-		return array;
-	}
-}
-
-	*/
